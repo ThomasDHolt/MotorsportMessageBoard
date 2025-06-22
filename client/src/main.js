1e340333db50e5c42c5a4c73bbe15df65e9a843b
ThomasDHolt <tholt9111@gmail.com>
2025-06-23 00:05:22 +0100

    Style the page

--- client/src/main.js
+++ client/src/main.js
@@ -15,57 +15,83 @@ async function DisplayMessages()
     const messages = await FetchMessages();
 
     messages.forEach((singleMessage) => {
-        const div = document.createElement("div");
+        const messageDiv = document.createElement("div");
+        const topDiv = document.createElement("div");
+        const mainDiv = document.createElement("div");
+        const bottomDiv = document.createElement("div");
+        const likeDiv = document.createElement("div");
+        const deleteDiv = document.createElement("div");
+
         const name = document.createElement("h3");
         const messageContent = document.createElement("p");
         const date = document.createElement("p");
         const rating = document.createElement("p");
         const likes = document.createElement("p");
         const likeButton = document.createElement("button");
-        const getButton = document.createElement("button");
+        const deleteButton = document.createElement("button");
 
-        div.append(name, messageContent, date, rating, likes, likeButton, getButton);
+        messageDiv.classList.add("flex-container", "flex-column", "message-container");
+        topDiv.classList.add("flex-container", "flex-row", "flex-space-between");
+        mainDiv.classList.add("flex-container", "flex-row", "flex-centre-horizontal");
+        bottomDiv.classList.add("flex-container", "flex-row", "flex-space-between");
+        likeDiv.classList.add("flex-container", "flex-row");
+
+        name.classList.add("name", "text-margin", "centre-text-horizontal");
+        rating.classList.add("text-margin", "centre-text-horizontal");
+        date.classList.add("text-margin", "centre-text-horizontal");
+        messageContent.classList.add("text-margin", "feedback-text");
+        likes.classList.add("text-margin", "extra-text");
+        
+
+        topDiv.append(name, rating, date);
+        mainDiv.append(messageContent);
+        likeDiv.append(likes, likeButton);
+        deleteDiv.append(deleteButton);
+        bottomDiv.append(likeDiv, deleteDiv);
+
+        messageDiv.append(topDiv, mainDiv, bottomDiv);
 
         const dateString = FormatDateStringForClient(new Date(singleMessage.date));
 
         name.innerText = singleMessage.sender;
         messageContent.innerText = singleMessage.content;
         date.innerText = dateString;
-        rating.innerText = singleMessage.rating;
+        rating.innerText = `${singleMessage.rating}/5`;
         likes.innerText = singleMessage.likes;
 
         likeButton.innerText = "Like";
-        likeButton.addEventListener("click", () => {LikeMessage(singleMessage, likes)});
+        likeButton.addEventListener("click", async () => {LikeMessage(singleMessage,)});
 
-        getButton.innerText = "Get";
-        getButton.addEventListener("click", () => {GetMessage(singleMessage)});
+        deleteButton.innerText = "Delete";
+        deleteButton.addEventListener("click", async () => {DeleteMessage(singleMessage)});
 
-        feedbackSection.appendChild(div);
+        feedbackSection.appendChild(messageDiv);
     });
 }
 
 DisplayMessages();
 
-feedbackForm.addEventListener("submit", (event) => {
+feedbackForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const formData = new FormData(feedbackForm);
     const jokeData = Object.fromEntries(formData);
 
-    fetch("https://nurburgring24guestbook-server.onrender.com/messages", {
+    await fetch("https://nurburgring24guestbook-server.onrender.com/messages", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(jokeData)
     });
+
+    feedbackSection.innerHTML = "";
+    DisplayMessages();
 });
 
-function LikeMessage(messageData, likesElement)
+async function LikeMessage(messageData)
 {
     const currentLikes = messageData.likes;
-
-    likesElement.innerText = currentLikes + 1;
 
     const requestBody =
     {
@@ -74,21 +100,27 @@ function LikeMessage(messageData, likesElement)
 
     console.log(messageData.likes);
 
-    fetch(`https://nurburgring24guestbook-server.onrender.com/messages/${messageData.msg_id}`, {
+    await fetch(`https://nurburgring24guestbook-server.onrender.com/messages/${messageData.msg_id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(requestBody)
     });
+
+    feedbackSection.innerHTML = "";
+    DisplayMessages();
 }
 
-function GetMessage(messageData)
+async function DeleteMessage(messageData)
 {
-    fetch(`https://nurburgring24guestbook-server.onrender.com/messages/${messageData.msg_id}`, {
-        method: "GET",
+    await fetch(`https://nurburgring24guestbook-server.onrender.com/messages/${messageData.msg_id}`, {
+        method: "DELETE",
         headers: {
             "Content-Type": "application/json"
         }
     });
+
+    feedbackSection.innerHTML = "";
+    DisplayMessages();
 }
